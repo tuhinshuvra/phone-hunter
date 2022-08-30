dfd59771839d80163d5fdf34da2b95312aa75d66
Tuhin Shuvra <tuhinshuvra18@gmail.com>
2022-08-30 12:01:59 +0600

    show all button added

--- script/phoneHunter.js
+++ script/phoneHunter.js
@@ -1,20 +1,28 @@
 
 
 
-const getPhones = async (searchText) => {
+const getPhones = async (searchText, dataLimit) => {
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     const response = await fetch(url);
     const phones = await response.json();
     // console.log(phones.data)
-    displayPhones(phones.data);
+    displayPhones(phones.data, dataLimit);
 
 }
-const displayPhones = phones => {
+const displayPhones = (phones, dataLimit) => {
     // console.log(phones);
 
     const phoneContainer = document.getElementById('phone-container');
     phoneContainer.textContent = '';
-    phones = phones.slice(0, 10);
+
+    const showAll = document.getElementById('show-all');
+
+    if (dataLimit && phones.length > 10) {
+        phones = phones.slice(0, 10);
+        showAll.classList.remove('d-none');
+    } else {
+        showAll.classList.add('d-none');
+    }
 
     const noPhone = document.getElementById('not-found-message');
     if (phones.length == 0) {
@@ -39,16 +47,36 @@ const displayPhones = phones => {
             </div>    
         `;
         phoneContainer.appendChild(phoneDiv);
-    })
+    });
+    toggleSpinner(false);
+
+}
+const searchProcess = (dataLimit) => {
+    toggleSpinner(true);
+    const searchField = document.getElementById('search-field');
+    searchText = searchField.value;
+    getPhones(searchText, dataLimit);
+    // searchField.value = '';
 }
 
 // phone search section start
 document.getElementById('btn-search').addEventListener('click', function () {
-    const searchField = document.getElementById('search-field');
-    searchText = searchField.value;
-    getPhones(searchText);
-    searchField.value = '';
+
+    searchProcess(10);
 })
 // phone search section end
+
+const toggleSpinner = isLoading => {
+    const loader = document.getElementById('loader');
+    if (isLoading) {
+        loader.classList.remove('d-none');
+    } else {
+        loader.classList.add('d-none');
+    }
+}
+
+document.getElementById('btn-show-all').addEventListener('click', function () {
+    searchProcess();
+})
 
 // getPhones();
